5f6c4ab6355be7c3a47c7246727c4d75b6b49f5f
Yunus ÖZCAN <me@yunusozcan.com>
2024-06-21 14:50:41 +0300

    storybook fix.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,18 @@
-// @ts-ignore
-import custom from "../webpack.config";
-import { StorybookConfig } from "@storybook/react-webpack5";
-import { Configuration as WebpackConfig } from 'webpack';
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  webpackFinal: async (config: WebpackConfig) => {
-    if (config.module) {
-      return {
-        ...config,
-        module: {
-          ...config.module,
-          rules: [...config.module.rules, ...custom.module.rules],
-        },
-      };
-    }
-    return config;
-  },
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: true,
-  },
 };
-
 export default config;
