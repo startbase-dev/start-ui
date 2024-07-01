3e7541451645a7335a4c70c0ac76043e69b825e7
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-07-01 12:43:16 +0300

    Add Divider stories

--- src/Divider/index.stories.tsx
+++ src/Divider/index.stories.tsx
@@ -43,6 +43,46 @@ DividerComponent.args = {
   children: "",
 };
 
+export const DividerWithVariant = Template.bind({});
+DividerWithVariant.args = {
+  title: "Divider with variant",
+  variant: "center",
+  orientation: "horizontal",
+  contentAlign: "center",
+  component: "div",
+  children: ""
+};
+
+export const DividerWithChildren = Template.bind({});
+DividerWithChildren.args = {
+  title: "Divider with children",
+  variant: "fullWidth",
+  orientation: "horizontal",
+  contentAlign: "center",
+  component: "div",
+  children: "Hello World"
+};
+
+export const DividerWithAlign = Template.bind({});
+DividerWithAlign.args = {
+  title: "Divider with children",
+  variant: "fullWidth",
+  orientation: "horizontal",
+  contentAlign: "start",
+  component: "div",
+  children: "Hello World"
+};
+
+export const DividerVertical = Template.bind({});
+DividerVertical.args = {
+  title: "Divider (vertical)",
+  variant: "fullWidth",
+  orientation: "vertical",
+  contentAlign: "center",
+  component: "div",
+  children: ""
+};
+
 const Component = {
   title: "UI/Divider",
   component: DividerComponent,
