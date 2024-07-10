12becf0fa11abe5efe8c4484662571c0763a0f0a
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-07-10 20:46:17 +0300

    Add break points prop

--- src/Grid/Column/Column.tsx
+++ src/Grid/Column/Column.tsx
@@ -6,45 +6,55 @@ interface ColumnProps extends React.AllHTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
   classNames?: string[];
   span?: number;
+  columns?: number;
   sm?: number;
   md?: number;
   lg?: number;
   xl?: number;
+  smMinWidth?: string;
+  mdMinWidth?: string;
+  lgMinWidth?: string;
+  xlMinWidth?: string;
   padding?: string;
 };
 
-function getWidth(span: number) {
-  return `${span / 12 * 100}%`;
+function getWidth(span: number, columns: number) {
+  return `${span / columns * 100}%`;
 };
 
 export default function Column({
   children,
   classNames = [],
   span = 12,
+  columns = 12,
   sm,
   md,
   lg,
   xl,
+  smMinWidth = "576px",
+  mdMinWidth = "768px",
+  lgMinWidth = "992px",
+  xlMinWidth = "1200px",
   style = {},
   padding = "8px",
   ...rest
 }: ColumnProps) {
-  const baseWidth = getWidth(span);
-  const smWidth = sm ? getWidth(sm) : baseWidth;
-  const mdWidth = md ? getWidth(md) : baseWidth;
-  const lgWidth = lg ? getWidth(lg) : baseWidth;
-  const xlWidth = xl ? getWidth(xl) : baseWidth;
+  const baseWidth = getWidth(span, columns);
+  const smWidth = sm ? getWidth(sm, columns) : baseWidth;
+  const mdWidth = md ? getWidth(md, columns) : baseWidth;
+  const lgWidth = lg ? getWidth(lg, columns) : baseWidth;
+  const xlWidth = xl ? getWidth(xl, columns) : baseWidth;
 
   return (
     <div
       className={clsx([s.root, ...classNames])}
       style={{
         ...style,
-        "--width": baseWidth,
-        "--width-sm": smWidth,
-        "--width-md": mdWidth,
-        "--width-lg": lgWidth,
-        "--width-xl": xlWidth,
+        width: baseWidth,
+        [`@media screen and (min-width: ${smMinWidth})`]: { width: smWidth },
+        [`@media screen and (min-width: ${mdMinWidth})`]: { width: mdWidth },
+        [`@media screen and (min-width: ${lgMinWidth})`]: { width: lgWidth },
+        [`@media screen and (min-width: ${xlMinWidth})`]: { width: xlWidth },
         "--padding": padding,
       }}
       {...rest}
