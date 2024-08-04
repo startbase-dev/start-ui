319ca6cdd50ba46e3c63f687dc46e5fc249caa54
ezgiergun <ezgiergun.k@gmail.com>
2024-08-04 15:55:27 +0300

    feat(ui): add Typography component, add new styles to global css for the new  Typograpghy component

--- src/Typography/index.stories.tsx
+++ src/Typography/index.stories.tsx
@@ -1,33 +1,133 @@
 import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
 import Typography from "./index";
 
-interface TemplateProps {
-  title: string;
-}
-
-const Template: React.FC<TemplateProps> = (props) => {
-  return (
-    <>
-      <h2>{props.title}</h2>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          flexDirection: "column",
-        }}
-      >
-        <Typography />
-      </div>
-    </>
-  );
-};
-
-export const TypographyComponent = Template.bind({});
-TypographyComponent.args = { title: "Typography" };
-
-const Component = {
+export default {
   title: "UI/Typography",
-  component: TypographyComponent,
+  component: Typography,
+  argTypes: {
+    variant: {
+      options: ["title", "subtitle", "paragraph", "muted", "quote"],
+      control: { type: "select" },
+    },
+    size: {
+      options: ["sm", "md", "lg", "lead"],
+      control: { type: "select" },
+    },
+    color: {
+      options: ["default", "info", "warning", "error", "success"],
+      control: { type: "select" },
+    },
+    decoration: {
+      options: ["none", "underline", "overline", "linethrough"],
+      control: { type: "select" },
+    },
+    elementType: {
+      options: ["h2", "h3", "h4", "h5", "h6"],
+      control: { type: "select" },
+    },
+    italic: { control: "boolean" },
+    bold: { control: "boolean" },
+    gradient: { control: "boolean" },
+    className: { control: "text" },
+  },
+  parameters: {
+    controls: {
+      include: [
+        "variant",
+        "size",
+        "color",
+        "decoration",
+        "gradient",
+        "italic",
+        "bold",
+        "className",
+      ],
+    },
+  },
+} as Meta<typeof Typography>;
+
+const Template: StoryFn<typeof Typography> = (args) => <Typography {...args} />;
+
+export const Title = Template.bind({});
+Title.args = {
+  variant: "title",
+  size: "lead",
+  color: "default",
+  children: "This is a Title",
+  bold: true,
+};
+Title.parameters = {
+  controls: {
+    exclude: ["elementType"],
+  },
 };
 
-export default Component;
+export const TitleWithGradient = Template.bind({});
+TitleWithGradient.args = {
+  variant: "title",
+  size: "lead",
+  color: "default",
+  children: "This is a Title",
+  bold: true,
+  gradient: true,
+};
+Title.parameters = {
+  controls: {
+    exclude: ["elementType"],
+  },
+};
+
+export const Subtitle = Template.bind({});
+Subtitle.args = {
+  variant: "subtitle",
+  elementType: "h4",
+  color: "info",
+  children: "This is a Subtitle",
+};
+Subtitle.parameters = {
+  controls: {
+    exclude: ["size"],
+  },
+};
+
+export const Paragraph = Template.bind({});
+Paragraph.args = {
+  variant: "paragraph",
+  size: "md",
+  color: "success",
+  children: "This is a Paragraph",
+  italic: true,
+};
+Paragraph.parameters = {
+  controls: {
+    exclude: ["elementType"],
+  },
+};
+
+export const Muted = Template.bind({});
+Muted.args = {
+  variant: "muted",
+  size: "md",
+  color: "default",
+  bold: true,
+  children: "This is Muted Text with Medium Size",
+};
+Muted.parameters = {
+  controls: {
+    exclude: ["elementType"],
+  },
+};
+
+export const Quote = Template.bind({});
+Quote.args = {
+  variant: "quote",
+  size: "lg",
+  color: "error",
+  children: `"This is a Quote"`,
+};
+Quote.parameters = {
+  controls: {
+    exclude: ["elementType"],
+  },
+};
--- src/Typography/index.tsx
+++ src/Typography/index.tsx
@@ -1,13 +1,251 @@
 import React, { forwardRef } from "react";
-
+import clsx from "clsx";
 import s from "./Typography.module.scss";
 
-interface IndexProps {}
+type TypographySize = "sm" | "md" | "lg" | "lead";
+type TypographyColor = "default" | "info" | "warning" | "error" | "success";
+type TypographyDecoration = "underline" | "overline" | "linethrough";
+type TypographyElement = "h2" | "h3" | "h4" | "h5" | "h6";
 
-const Index = forwardRef<HTMLDivElement, IndexProps>((props, ref) => {
-  return <div className={s.root} ref={ref} {...props}></div>;
+interface BaseTypographyProps {
+  color?: TypographyColor;
+  decoration?: TypographyDecoration;
+  italic?: boolean;
+  bold?: boolean;
+  gradient?: boolean;
+  children: React.ReactNode;
+  className?: string;
+}
+
+interface TitleProps extends BaseTypographyProps {
+  size?: TypographySize;
+}
+
+interface SubtitleProps extends BaseTypographyProps {
+  elementType?: TypographyElement;
+}
+
+interface ParagraphProps extends BaseTypographyProps {
+  size?: TypographySize;
+}
+
+interface MutedProps extends BaseTypographyProps {
+  size?: TypographySize;
+}
+
+interface QuoteProps extends BaseTypographyProps {
+  size?: TypographySize;
+}
+
+type CombinedTypographyProps = TitleProps &
+  SubtitleProps &
+  ParagraphProps &
+  MutedProps &
+  QuoteProps;
+
+const Title = forwardRef<HTMLHeadingElement, TitleProps>((props, ref) => {
+  const {
+    size = "lead",
+    color = "default",
+    decoration = false,
+    italic = false,
+    bold = false,
+    gradient = false,
+    children,
+    className,
+    ...rest
+  } = props;
+
+  const typographyClass: string = clsx(
+    s.root,
+    s.title,
+    s[size],
+    s[color],
+    {
+      [s[decoration || ""]]: decoration,
+      [s.italic]: italic,
+      [s.bold]: bold,
+      [s.gradient]: gradient,
+    },
+    className,
+  );
+
+  return (
+    <h1 className={typographyClass} ref={ref} {...rest}>
+      {children}
+    </h1>
+  );
 });
+Title.displayName = "Title";
 
-Index.displayName = "Typography";
+const Subtitle = forwardRef<HTMLHeadingElement, SubtitleProps>((props, ref) => {
+  const {
+    color = "default",
+    decoration = false,
+    italic = false,
+    bold = false,
+    gradient = false,
+    elementType = "h3",
+    children,
+    className,
+    ...rest
+  } = props;
 
-export default Index;
+  const typographyClass: string = clsx(
+    s.root,
+    s.subtitle,
+    s[color],
+    {
+      [s[decoration || ""]]: decoration,
+      [s.italic]: italic,
+      [s.bold]: bold,
+      [s.gradient]: gradient,
+    },
+    className,
+  );
+
+  const Component = elementType;
+
+  return (
+    <Component className={typographyClass} ref={ref} {...rest}>
+      {children}
+    </Component>
+  );
+});
+Subtitle.displayName = "Subtitle";
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  (props, ref) => {
+    const {
+      size = "sm",
+      color = "default",
+      decoration = false,
+      italic = false,
+      bold = false,
+      gradient = false,
+      children,
+      className,
+      ...rest
+    } = props;
+
+    const typographyClass: string = clsx(
+      s.root,
+      s.paragraph,
+      s[size],
+      s[color],
+      {
+        [s[decoration || ""]]: decoration,
+        [s.italic]: italic,
+        [s.bold]: bold,
+        [s.gradient]: gradient,
+      },
+      className,
+    );
+
+    return (
+      <p className={typographyClass} ref={ref} {...rest}>
+        {children}
+      </p>
+    );
+  },
+);
+Paragraph.displayName = "Paragraph";
+const Muted = forwardRef<HTMLParagraphElement, MutedProps>((props, ref) => {
+  const {
+    size = "md",
+    color = "default",
+    decoration = false,
+    italic = false,
+    bold = false,
+    gradient = false,
+    children,
+    className,
+    ...rest
+  } = props;
+
+  const typographyClass: string = clsx(
+    s.root,
+    s.muted,
+    s[size],
+    s[color],
+    {
+      [s[decoration || ""]]: decoration,
+      [s.italic]: italic,
+      [s.bold]: bold,
+      [s.gradient]: gradient,
+    },
+    className,
+  );
+
+  return (
+    <p className={typographyClass} ref={ref} {...rest}>
+      {children}
+    </p>
+  );
+});
+Muted.displayName = "Muted";
+
+const Quote = forwardRef<HTMLQuoteElement, QuoteProps>((props, ref) => {
+  const {
+    size = "sm",
+    color = "default",
+    decoration = false,
+    italic = false,
+    bold = false,
+    gradient = false,
+    children,
+    className,
+    ...rest
+  } = props;
+
+  const typographyClass: string = clsx(
+    s.root,
+    s.quote,
+    s[size],
+    s[color],
+    {
+      [s[decoration || ""]]: decoration,
+      [s.italic]: italic,
+      [s.bold]: bold,
+      [s.gradient]: gradient,
+    },
+    className,
+  );
+
+  return (
+    <blockquote className={typographyClass} ref={ref} {...rest}>
+      {children}
+    </blockquote>
+  );
+});
+Quote.displayName = "Quote";
+
+type TypographyVariant = "title" | "subtitle" | "paragraph" | "muted" | "quote";
+
+interface TypographyControllerProps extends CombinedTypographyProps {
+  variant: TypographyVariant;
+}
+
+const Typography: React.FC<TypographyControllerProps> = ({
+  variant,
+  ...props
+}) => {
+  switch (variant) {
+    case "title":
+      return <Title {...props} />;
+    case "subtitle":
+      return <Subtitle {...props} />;
+    case "paragraph":
+      return <Paragraph {...props} />;
+    case "muted":
+      return <Muted {...props} />;
+    case "quote":
+      return <Quote {...props} />;
+    default:
+      return null;
+  }
+};
+
+Typography.displayName = "Typography";
+
+export default Typography;
