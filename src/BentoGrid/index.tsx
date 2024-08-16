f8c48e9c625074265ad2b4b42cc5ff994733ea0a
Emre Onursoy <emreonursoy@gmail.com>
2024-08-17 01:19:44 +0300

    feat: too many changes including path and landing

--- src/BentoGrid/index.tsx
+++ src/BentoGrid/index.tsx
@@ -1,11 +1,26 @@
 import React, { forwardRef } from "react";
-
 import s from "./BentoGrid.module.scss";
+import { Props } from "./types";
 
-interface IndexProps {}
-
-const Index = forwardRef<HTMLDivElement, IndexProps>((props, ref) => {
-  return <div className={s.root} ref={ref} {...props}></div>;
+const Index = forwardRef<HTMLDivElement, Props>(({ items, ...rest }, ref) => {
+  return (
+    <div className={s.root} ref={ref} {...rest}>
+      <div className={s.libraries}>
+        {items?.map((item, index) => (
+          <div
+            key={index}
+            className={s.library}
+            style={{
+              gridColumn: `span ${item.width}`,
+              gridRow: `span ${item.height}`,
+            }}
+          >
+            {item.element}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 });
 
 Index.displayName = "BentoGrid";
