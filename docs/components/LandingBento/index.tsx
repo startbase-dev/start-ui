d8a8b14f6165d21753b69a47a03e6fe7643c1ea9
Emre Onursoy <emreonursoy@gmail.com>
2024-08-31 11:19:57 +0300

    feat: landing design

--- docs/components/LandingBento/index.tsx
+++ docs/components/LandingBento/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BentoGrid, BentoCard } from "@start-base/start-ui";
 import "@start-base/start-ui/styles.css";
 import style from "./style.module.css";
+import VideoPlayer from "@components/VideoPlayer/VideoPlayer";
 
 const LandingBento: React.FC = () => {
   return (
@@ -11,95 +12,172 @@ const LandingBento: React.FC = () => {
         items={[
           {
             element: (
+              <BentoCard
+                title="Unleash Creativity with 50+ components"
+                isTitleVisibleInSecond={false}
+                firstContent={
+                  <div className={style.cardContent}>
+                    <VideoPlayer
+                      isLoopActive
+                      autoPlay
+                      muted
+                      url="https://ficzfphhbhoykqsfygfq.supabase.co/storage/v1/object/public/files/3ab23e1e-8ab9-4a98-a86a-92aceb374a42.mp4"
+                    />
+                  </div>
+                }
+                secondContent={
+                  <div>
+                    Unlock a universe of possibilities with over 50 components
+                    designed to empower your web projects. From minimalistic
+                    designs to bold statements, Start UI is your toolkit for
+                    crafting digital experiences that stand out
+                  </div>
+                }
+                rootClassName={style.cardRoot}
+              />
+            ),
+            width: 3,
+            height: 2,
+          },
+          {
+            element: (
+              <BentoCard
+                firstContent={
+                  <div className={style.cardContent}>
+                    <div style={{ padding: "40px", fontSize: "32px" }}>
+                      Build Your Foundation
+                    </div>
+                  </div>
+                }
+                secondContent={
+                  <div style={{ padding: "40px" }}>
+                    Start with the essentials. Our basic components are the
+                    backbone of your next big idea—engineered for simplicity,
+                    designed for impact.
+                  </div>
+                }
+                rootClassName={style.cardRoot}
+              />
+            ),
+            width: 1,
+            height: 2,
+          },
+          {
+            element: (
+              <BentoCard
+                isTitleVisibleInSecond={false}
+                firstContent={
+                  <div className={style.cardContent}>
+                    <div style={{ padding: "40px", fontSize: "32px" }}>
+                      Transform Your Workflow
+                    </div>
+                  </div>
+                }
+                secondContent={
+                  <div style={{ padding: "40px" }}>
+                    Comprehensive, clear, and always here for you. Our
+                    documentation and support ensure you’re never alone on your
+                    journey to greatness
+                  </div>
+                }
+                rootClassName={style.cardRoot}
+              />
+            ),
+            width: 1,
+            height: 1,
+          },
+          {
+            element: (
               <div>
                 <BentoCard
                   firstContent={
-                    <>
+                    <div className={style.cardContent}>
                       <div style={{ padding: "40px", fontSize: "32px" }}>
-                        Unleash Creativity with 50+ components
+                        Startbase Studio is where ideas come to life,
+                        effortlessly bridging the gap between creativity and
+                        execution.Design, develop, and deploy all in one place.
                       </div>
-                      <img src="https://nextra.site/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcard-1.dark.efd5c446.png&w=3840&q=75" />
-                    </>
+                    </div>
+                  }
+                  redirectTo="https://www.startbase.dev/studio"
+                  rootClassName={style.cardRoot}
+                />
+              </div>
+            ),
+            width: 2,
+            height: 2,
+          },
+          {
+            element: (
+              <div>
+                <BentoCard
+                  title="Your Fast-Track to Success"
+                  firstContent={
+                    <div className={style.cardContent}>
+                      <VideoPlayer
+                        isLoopActive
+                        autoPlay
+                        muted
+                        url="https://ficzfphhbhoykqsfygfq.supabase.co/storage/v1/object/public/files/6ec44b5d-7e04-462f-80d5-984e7543bdfb.mp4"
+                      />
+                    </div>
+                  }
+                  redirectTo="https://www.startbase.dev/boilerplates"
+                  rootClassName={style.cardRoot}
+                />
+              </div>
+            ),
+            width: 2,
+            height: 2,
+          },
+          {
+            element: (
+              <div>
+                <BentoCard
+                  firstContent={
+                    <div className={style.cardContent}>
+                      <div style={{ padding: "40px", fontSize: "32px" }}>
+                        Capture Attention, Drive Action
+                      </div>
+                    </div>
                   }
                   secondContent={
                     <div style={{ padding: "40px" }}>
-                      Unleash Your Creative Power Description: Unlock a universe
-                      of possibilities with over 50 components designed to
-                      empower your web projects. From minimalistic designs to
-                      bold statements, Start UI is your toolkit for crafting
-                      digital experiences that stand out
+                      Your brand deserves to shine. With our marketing
+                      components, you can craft eye-catching visuals that engage
+                      and convert. From banners to pop-ups, these elements are
+                      designed to capture attention and inspire action, helping
+                      you achieve your marketing goals.
                     </div>
                   }
                   rootClassName={style.cardRoot}
                 />
               </div>
             ),
-            width: 2,
-            height: 10,
-          },
-          {
-            element: (
-              <div style={{ backgroundColor: "lightgreen", padding: "12px" }}>
-                Item 2
-              </div>
-            ),
             width: 1,
             height: 1,
           },
+
           {
             element: (
-              <div style={{ backgroundColor: "lightcoral", padding: "12px" }}>
-                Item 3
-              </div>
-            ),
-            width: 1,
-            height: 1,
-          },
-          {
-            element: (
-              <div
-                style={{
-                  backgroundColor: "lightgoldenrodyellow",
-                  padding: "12px",
-                }}
-              >
-                Item 4
-              </div>
-            ),
-            width: 1,
-            height: 1,
-          },
-          {
-            element: (
-              <div style={{ backgroundColor: "lightcoral", padding: "12px" }}>
-                Item 5
-              </div>
-            ),
-            width: 3,
-            height: 1,
-          },
-          {
-            element: (
-              <div style={{ backgroundColor: "lightblue", padding: "12px" }}>
-                Item 6
-              </div>
-            ),
-            width: 1,
-            height: 1,
-          },
-          {
-            element: (
-              <div style={{ backgroundColor: "lightgreen", padding: "12px" }}>
-                Item 7
-              </div>
-            ),
-            width: 2,
-            height: 1,
-          },
-          {
-            element: (
-              <div style={{ backgroundColor: "lightcoral", padding: "12px" }}>
-                Item 8
+              <div>
+                <BentoCard
+                  firstContent={
+                    <div className={style.cardContent}>
+                      <div style={{ padding: "40px", fontSize: "32px" }}>
+                        Build Your Foundation
+                      </div>
+                    </div>
+                  }
+                  secondContent={
+                    <div style={{ padding: "40px" }}>
+                      Start with the essentials. Our basic components are the
+                      backbone of your next big idea—engineered for simplicity,
+                      designed for impact.
+                    </div>
+                  }
+                  rootClassName={style.cardRoot}
+                />
               </div>
             ),
             width: 1,
---
+++ docs/components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,123 @@
+import React, {
+  useRef,
+  useEffect,
+  useCallback,
+  useState,
+  useMemo,
+} from "react";
+import s from "./VideoPlayer.module.scss";
+import Replay from "./Replay";
+import Pause from "./Pause";
+import Play from "./Play";
+
+interface VideoPlayerProps {
+  url: string;
+  isLoopActive?: boolean;
+  autoPlay?: boolean;
+  muted?: boolean;
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({
+  url,
+  isLoopActive = false,
+  autoPlay = false,
+  muted = false,
+}) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isEnded, setIsEnded] = useState(false);
+
+  useEffect(() => {
+    const handlePlay = () => setIsPlaying(true);
+    const handlePause = () => setIsPlaying(false);
+    const handleEnded = () => {
+      setIsPlaying(false);
+      setIsEnded(true);
+    };
+
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener("play", handlePlay);
+      video.addEventListener("pause", handlePause);
+      video.addEventListener("ended", handleEnded);
+    }
+
+    return () => {
+      if (video) {
+        video.removeEventListener("play", handlePlay);
+        video.removeEventListener("pause", handlePause);
+        video.removeEventListener("ended", handleEnded);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (autoPlay) {
+      setIsPlaying(true);
+    }
+  }, []);
+
+  const handleReplay = useCallback(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.currentTime = 0;
+      video.play();
+      setIsEnded(false);
+    }
+  }, []);
+
+  const handlePlayPause = useCallback(() => {
+    const video = videoRef.current;
+    if (video) {
+      if (isPlaying) {
+        video.pause();
+      } else {
+        video.play();
+      }
+    }
+  }, [isPlaying]);
+
+  const button = useMemo(() => {
+    if (isEnded) {
+      return (
+        <button className={s.button} onClick={handleReplay}>
+          Replay <Replay width={16} />
+        </button>
+      );
+    } else {
+      return (
+        <button className={s.button} onClick={handlePlayPause}>
+          {isPlaying ? (
+            <>
+              Pause <Pause width={16} />
+            </>
+          ) : (
+            <>
+              Play <Play width={16} />
+            </>
+          )}
+        </button>
+      );
+    }
+  }, [isEnded, isPlaying]);
+
+  return (
+    <div className={s.container}>
+      <video
+        ref={videoRef}
+        className={s.player}
+        autoPlay={autoPlay}
+        width="100%"
+        height="100%"
+        src={url}
+        loop={isLoopActive}
+        muted={muted}
+      >
+        Your browser does not support the video tag.
+      </video>
+      {button}
+    </div>
+  );
+};
+
+export default VideoPlayer;
