9b8cd45c1e9a4091b5f89e70935bf03953e72f6b
ezgiergun <ezgiergun.k@gmail.com>
2024-09-04 00:40:55 +0300

    feat: add toggle component with its storybook

--- src/Toggle/index.tsx
+++ src/Toggle/index.tsx
@@ -1,13 +1,77 @@
-import React, { forwardRef } from "react";
-
+import React, {
+  ButtonHTMLAttributes,
+  forwardRef,
+  useState,
+  ReactNode,
+} from "react";
+import cx from "clsx";
+// eslint-disable-next-line css-modules/no-unused-class
 import s from "./Toggle.module.scss";
+import "../style/components/toggle.scss";
 
-interface IndexProps {}
+type ToggleProps = {
+  initialState?: boolean;
+  onToggle?: (state: boolean) => void;
+  color?: "primary" | "secondary" | "destructive";
+  variant?: "default" | "outline";
+  size?: "small" | "medium" | "large";
+  fluid?: boolean;
+  className?: string;
+  disabled?: boolean;
+  children?: ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Index = forwardRef<HTMLDivElement, IndexProps>((props, ref) => {
-  return <div className={s.root} ref={ref} {...props}></div>;
-});
+const Toggle = forwardRef<HTMLButtonElement, ToggleProps>(
+  (
+    {
+      initialState = false,
+      onToggle,
+      color = "primary",
+      variant = "default",
+      size = "medium",
+      fluid = false,
+      className = "",
+      disabled = false,
+      children,
+      ...rest
+    },
+    ref,
+  ) => {
+    const [isToggled, setIsToggled] = useState(initialState);
 
-Index.displayName = "Toggle";
+    const handleClick = () => {
+      if (disabled) return;
+      const newState = !isToggled;
+      setIsToggled(newState);
+      if (onToggle) {
+        onToggle(newState);
+      }
+    };
 
-export default Index;
+    return (
+      <button
+        ref={ref}
+        type="button"
+        className={cx(
+          s.root,
+          s[variant],
+          s[color],
+          s[size],
+          fluid && s.fluid,
+          isToggled ? s.toggled : s.untoggled,
+          disabled && s.disabled,
+          className,
+        )}
+        onClick={handleClick}
+        disabled={disabled}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+
+Toggle.displayName = "Toggle";
+
+export default Toggle;
