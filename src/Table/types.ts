68c1ff2f8fbf20ea1323e9d142a2869ac2913b03
Emre Onursoy <emreonursoy@gmail.com>
2024-09-04 00:49:53 +0300

    feat: table component

---
+++ src/Table/types.ts
@@ -0,0 +1,33 @@
+import {
+  HTMLAttributes,
+  TdHTMLAttributes,
+  CSSProperties,
+  ReactNode,
+} from "react";
+import { Key } from "react";
+
+export interface DataItem {
+  key?: Key;
+  [key: string]: unknown;
+}
+
+export interface CustomColumnType {
+  title: ReactNode;
+  dataIndex?: string | number;
+  key?: Key;
+  render?: (value: unknown, record: DataItem, index: number) => React.ReactNode;
+  onCell?: (
+    record: DataItem,
+    rowIndex: number,
+  ) => HTMLAttributes<HTMLElement> & TdHTMLAttributes<HTMLElement>;
+}
+
+export interface TableProps {
+  data?: DataItem[];
+  columns: CustomColumnType[];
+  hidePagination?: boolean;
+  rowClassName?: string;
+  emptyText?: ReactNode;
+  summary?: (data: readonly DataItem[]) => ReactNode;
+  style?: CSSProperties;
+}
