ce355a2bf3a44d10ecbed19465cbb0a2eeb40319
Yunus ÖZCAN <me@yunusozcan.com>
2024-09-29 15:07:56 +0300

    reviews.

--- src/floatings/FloatingMenuItem/types.ts
+++ src/floatings/FloatingMenuItem/types.ts
@@ -3,7 +3,7 @@ import type { AllHTMLAttributes } from 'react';
 export interface FloatingMenuItemProps
   extends AllHTMLAttributes<HTMLDivElement> {
   label: string;
-  className: string;
+  className?: string;
   disabled?: boolean;
   onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
   onFocus?: (event: React.FocusEvent<HTMLDivElement>) => void;
