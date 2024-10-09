96a5f0b798635df61c62d05bf76deaf60d030634
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-10-09 10:12:39 +0300

    run lint lint:docs format

--- docs/components/ModalExample/ModalExample.tsx
+++ docs/components/ModalExample/ModalExample.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import { GlobalModals, Modal, useModal } from '@start-base/react-modal';
 import Button from '@start-base/start-ui/Button';
 
@@ -10,21 +11,13 @@ export default function Modals() {
   return (
     <GlobalModals>
       <div>
-        <Button onClick={() => openModal("modal")}>
-          Modal
-        </Button>
-        <Button onClick={() => openModal("left-sidebar")}>
-          Left Sidebar
-        </Button>
-        <Button onClick={() => openModal("right-sidebar")}>
+        <Button onClick={() => openModal('modal')}>Modal</Button>
+        <Button onClick={() => openModal('left-sidebar')}>Left Sidebar</Button>
+        <Button onClick={() => openModal('right-sidebar')}>
           Right Sidebar
         </Button>
-        <Button onClick={() => openModal("drawer")}>
-          Drawer
-        </Button>
-        <Button onClick={() => openModal("full-page")}>
-          Full Page
-        </Button>
+        <Button onClick={() => openModal('drawer')}>Drawer</Button>
+        <Button onClick={() => openModal('full-page')}>Full Page</Button>
       </div>
       <Modal name="modal" isOpen={false}>
         <div>
@@ -40,12 +33,14 @@ export default function Modals() {
       >
         <div>
           <h5>Left Sidebar</h5>
-          <Button onClick={() => closeModal('left-sidebar')}>
-            Close
-          </Button>
+          <Button onClick={() => closeModal('left-sidebar')}>Close</Button>
         </div>
       </Modal>
-      <Modal name="right-sidebar" className={styles.rightSidebar} isOpen={false}>
+      <Modal
+        name="right-sidebar"
+        className={styles.rightSidebar}
+        isOpen={false}
+      >
         <div>
           <h5>Right Sidebar</h5>
         </div>
@@ -59,17 +54,13 @@ export default function Modals() {
       >
         <div>
           <h5>Drawer</h5>
-          <Button onClick={() => closeModal('drawer')}>
-            Close
-          </Button>
+          <Button onClick={() => closeModal('drawer')}>Close</Button>
         </div>
       </Modal>
       <Modal name="full-page" className={styles.fullPage} isOpen={false}>
         <div>
           <h5>Full Page</h5>
-          <Button onClick={() => closeModal('full-page')}>
-            Close
-          </Button>
+          <Button onClick={() => closeModal('full-page')}>Close</Button>
         </div>
       </Modal>
     </GlobalModals>
