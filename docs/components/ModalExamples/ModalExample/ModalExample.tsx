6d10a3e39782b048119ecad6df956f934f033d61
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-10-10 10:52:01 +0300

    add several improvements to documentation pages

---
+++ docs/components/ModalExamples/ModalExample/ModalExample.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Modal } from "@start-base/react-modal";
+import styles from "./ModalExample.module.scss";
+
+export default function ModalExample() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      <Modal name="local-modal-example" isOpen={isOpen} className={styles.modal} overlayClassName={styles.overlay} onRequestClose={() => setIsOpen(false)}>
+        <h1>Hello World</h1>
+      </Modal>
+    </div>
+  );
+};
