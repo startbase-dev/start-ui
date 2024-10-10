9d00198ecca77d1d5dc27ec70be50d01739c36f7
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-10-10 10:59:49 +0300

    run lint lint:docs format

--- docs/components/ModalExamples/ModalExample/ModalExample.tsx
+++ docs/components/ModalExamples/ModalExample/ModalExample.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
-import { Modal } from "@start-base/react-modal";
-import styles from "./ModalExample.module.scss";
+import React, { useState } from 'react';
+import { Modal } from '@start-base/react-modal';
+import styles from './ModalExample.module.scss';
 
 export default function ModalExample() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <button onClick={() => setIsOpen(true)}>Open Modal</button>
-      <Modal name="local-modal-example" isOpen={isOpen} className={styles.modal} overlayClassName={styles.overlay} onRequestClose={() => setIsOpen(false)}>
+      <Modal
+        name="local-modal-example"
+        isOpen={isOpen}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+        onRequestClose={() => setIsOpen(false)}
+      >
         <h1>Hello World</h1>
       </Modal>
     </div>
   );
-};
+}
