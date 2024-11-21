184a757af0a11b019e08b2c99cb7f406ed7b05be
Kağan Ahmet Okan <okaganahmet@gmail.com>
2024-11-21 18:26:50 +0300

    update divider docs

--- src/Divider/index.tsx
+++ src/Divider/index.tsx
@@ -3,40 +3,45 @@ import styles from './Divider.module.scss';
 import clsx from 'clsx';
 import type { DividerProps } from './types';
 
-const Divider = forwardRef<HTMLDivElement, DividerProps>(({
-  children,
-  className,
-  variant = "full",
-  orientation = "horizontal",
-  align = "center",
-  color = "var(--sui-border)",
-  size = "1px",
-  round = false,
-  style = {},
-  ...props
-}, ref) => {
-  const rootClassNames = clsx(className, styles.root);
+const Divider = forwardRef<HTMLDivElement, DividerProps>(
+  (
+    {
+      children,
+      className,
+      variant = 'full',
+      orientation = 'horizontal',
+      align = 'center',
+      color = 'var(--sui-border)',
+      size = '1px',
+      round = false,
+      style = {},
+      ...props
+    },
+    ref
+  ) => {
+    const rootClassNames = clsx(className, styles.root);
 
-  return (
-    <div
-      className={rootClassNames}
-      role='separator'
-      aria-orientation={orientation}
-      data-variant={variant}
-      data-align={align}
-      data-round={`${round}`}
-      ref={ref}
-      style={{
-        "--sui-divider-color": color,
-        "--sui-divider-size": size,
-        ...style
-      }}
-      {...props}
-    >
-      {children}
-    </div>
-  )
-});
+    return (
+      <div
+        className={rootClassNames}
+        role="separator"
+        aria-orientation={orientation}
+        data-variant={variant}
+        data-align={align}
+        data-round={`${round}`}
+        ref={ref}
+        style={{
+          '--sui-divider-color': color,
+          '--sui-divider-size': size,
+          ...style,
+        }}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+);
 
 Divider.displayName = 'Divider';
 
--- src/Divider/types.ts
+++ src/Divider/types.ts
@@ -1,7 +1,7 @@
 export interface DividerProps extends React.HTMLAttributes<HTMLDivElement> {
-  variant?: "full" | "start" | "center" | "end";
-  orientation?: "horizontal" | "vertical";
-  align?: "start" | "center" | "end";
+  variant?: 'full' | 'start' | 'center' | 'end';
+  orientation?: 'horizontal' | 'vertical';
+  align?: 'start' | 'center' | 'end';
   color?: string;
   size?: `${string}px` | `${string}rem`;
   round?: boolean;
